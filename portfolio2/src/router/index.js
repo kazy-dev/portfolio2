eb94ad020caaad66c6f93831d0597d0e8e945124
kazy <yoshikazu@y-macbook-pro-work.local>
2019-07-30 19:21:09 +0900

    テスト用のvue cli関連を追加。

---
+++ portfolio2/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import page1 from '@/components/page1'
+import page2 from '@/components/page2'
+import HelloWorld from '@/components/HelloWorld'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: page1
+    },
+    {
+      path: '/page2',
+      component: page2
+    }
+  ]
+})
